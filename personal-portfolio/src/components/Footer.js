069ba2822819e0ec063d988e5b7e786826452e14
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2024-01-27 21:14:31 +0545

    update: logo and profile links

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import logo from "../assets/img/aaryash-logo-white.png";
+import navLinkedin from '../assets/img/nav-linkedin.svg';
+import navGithub from '../assets/img/nav-github.svg';
+import navInstagram from '../assets/img/nav-instagram.svg';
 
 export const Footer = () => {
   return (
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a href="https://www.linkedin.com/in/aaryash-shakya/" target="_blank" rel="noreferrer"><img src={navLinkedin} alt="Linkedin logo" /></a>
+                <a href="https://github.com/Aaryash-Shakya" target="_blank" rel="noreferrer"><img src={navGithub} alt="Github logo" /></a>
+                <a href="#"><img src={navInstagram} alt="Instagram logo" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import logo from '../assets/img/aaryash-logo-white.png';
+import navLinkedin from '../assets/img/nav-linkedin.svg';
+import navGithub from '../assets/img/nav-github.svg';
+import navInstagram from '../assets/img/nav-instagram.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/aaryash-shakya/" target="_blank" rel="noreferrer"><img src={navLinkedin} alt="Linkedin logo" /></a>
+                <a href="https://github.com/Aaryash-Shakya" target="_blank" rel="noreferrer"><img src={navGithub} alt="Github logo" /></a>
+                <a href="#"><img src={navInstagram} alt="Instagram logo" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
